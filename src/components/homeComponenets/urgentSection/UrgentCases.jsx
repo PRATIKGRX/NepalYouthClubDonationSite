ee2775aaf43b61f534eb34bb813935b8b3fce2b7
Pratik <pratikgrx06@gmail.com>
2025-10-16 11:15:00 +0545

    update the json to js

--- src/components/homeComponenets/urgentSection/UrgentCases.jsx
+++ src/components/homeComponenets/urgentSection/UrgentCases.jsx
@@ -1,7 +1,7 @@
 
 import Card from "./Card";
 import Button from "./Button";
-import data from "../../../data/urgentCases.json";
+import data from "../../../data/urgentCases";
 
 const UrgentCases = () => {
   const featured = data[0];
