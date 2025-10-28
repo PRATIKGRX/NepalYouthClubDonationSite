611b33999b334698ea6ef58f8c680f3646b8f4e5
Manisha Lama <manishalama136@gmail.com>
2025-10-28 22:22:38 +0545

    typo

--- src/components/homeComponenets/gallerySection/Gallery.jsx
+++ src/components/homeComponenets/gallerySection/Gallery.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef, use } from "react";
+import { useEffect, useState, useCallback, useRef} from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import galleryimg from "../../../assets/galleryimg.jpg";
