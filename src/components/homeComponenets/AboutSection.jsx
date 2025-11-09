2157217f230b0127f4ff4777269020c482082005
Manisha Lama <manishalama136@gmail.com>
2025-11-09 16:26:23 +0545

    stats section responsive

--- src/components/homeComponenets/AboutSection.jsx
+++ src/components/homeComponenets/AboutSection.jsx
@@ -3,13 +3,13 @@ export const AboutSection = () => {
   return (
     <div>
       {/* Heading */}
-      <section className="py-16">
-        <h1 className="text-[15px] md:text-3xl lg:text-[36px] mb-6 font-medium uppercase lg:normal-case ">About Nepal Youth Club</h1>
+      <section className="py-4 lg:py-16">
+        <h1 className="text-[15px] md:text-3xl lg:text-[36px] mb-3 lg:mb-6 font-medium uppercase lg:normal-case ">About Nepal Youth Club</h1>
 
         {/* Two Column Layout */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-8 md:gap-12">
           {/* Left Content */}
-          <div className="flex flex-col gap-8 lg:order-1">
+          <div className="flex flex-col gap-4 lg:gap-8 lg:order-1">
             <p className="text-gray-700 text-justify">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -38,27 +38,27 @@ export const AboutSection = () => {
       </section>
 
       {/* Stats Section */}
-      <section className="py-16">
+      <section className="py-4 lg:py-16">
         <div
-          className="w-full grid grid-cols-3 h-[217px] rounded-[5px] overflow-hidden"
+          className="w-full grid grid-cols-3 lg:h-[217px] rounded-[5px] overflow-hidden"
           style={{ boxShadow: "5px 5px 9px 0px rgba(0,56,147,0.25)" }}
         >
           {/* Stat 1 */}
-          <div className="flex flex-col items-center justify-center border border-[#00389380] h-full rounded-l-[5px]">
-            <div className="text-[50px] font-semibold ">50+</div>
-            <div className="text-[22px]">Volunteers</div>
+          <div className="flex flex-col px-4 py-3 items-center justify-center border border-[#00389380] h-full rounded-l-[5px]">
+            <div className="text-2xl lg:text-[50px] font-semibold ">50+</div>
+            <div className="text-xs lg:text-[22px]">Volunteers</div>
           </div>
 
           {/* Stat 2 */}
           <div className="flex flex-col items-center justify-center border border-[#00389380] h-full">
-            <div className="text-[50px] font-semibold ">200+</div>
-            <div className="text-[22px]">Life Helped</div>
+            <div className="text-2xl lg:text-[50px] font-semibold ">200+</div>
+            <div className="text-xs lg:text-[22px]">Life Helped</div>
           </div>
 
           {/* Stat 3 */}
           <div className="flex flex-col items-center justify-center border border-[#00389380] h-full rounded-r-[5px]">
-            <div className="text-[50px] font-semibold ">10+</div>
-            <div className="text-[22px]">Projects</div>
+            <div className="text-2xl lg:text-[50px] font-semibold ">10+</div>
+            <div className="text-base lg:text-[22px]">Projects</div>
           </div>
         </div>
       </section>
